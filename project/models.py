542a3550088f94e90930b9a6c3431a2a77c2158c
NDimish <nathanmani@outlook.com>
2022-12-09 15:17:58 +0000

    made database

--- project/models.py
+++ project/models.py
@@ -1,3 +1,13 @@
+from django.core.validators import MinValueValidator, MaxValueValidator
 from django.db import models
+from django.db.models import Max
 
 # Create your models here.
+
+
+class Term(models.Model):
+    name = models.CharField(max_length=30, null = False)
+    name = models.CharField(max_length=120, null = False)
+    quantity =  models.IntegerField(validators=[
+        MinValueValidator(0),
+        MaxValueValidator(100)])
